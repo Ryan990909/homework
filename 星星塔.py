68d93b6623b844bd08c82497e9b4b10cafc8c668
Ryan990909 <86942073+Ryan990909@users.noreply.github.com>
2021-07-08 13:16:00 +0800

    Add files via upload

---
+++ 星星塔.py
@@ -0,0 +1,14 @@
+print('**********')
+print('*********')
+print('********')
+print('*******')
+print('******')
+print('*****')
+print('****')
+print('***')
+print('**')
+print('*')
+
+
+for i in range(1,10): 
+    print('*' *i)
