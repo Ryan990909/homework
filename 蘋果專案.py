1b225fadf02b9e587be8625da9e6e6237e9dca5f
Ryan990909 <86942073+Ryan990909@users.noreply.github.com>
2021-07-14 14:48:19 +0800

    Add files via upload

---
+++ 蘋果專案.py
@@ -0,0 +1,27 @@
+time = 0
+apple = (int(input("請問有幾顆蘋果:"))) 
+cost = (int(input("請問一顆蘋果多少錢:")))
+money = 200 
+deal = 0
+spend (cost * 2) 
+while time < 1:
+ q = (int(input("請問要用甚麼功能？1設定 2進貨 3出貨"))
+if q == 1
+    apple = (int(input("請問有幾顆蘋果:"))) 
+    cost = (int(input("請問一個蘋果多少錢:")))
+    spend = cost * 2
+if q == 2
+    in_apple = (int(input("請輸入今天進貨幾顆蘋果")))
+    money = money - (apple * cost) 
+    apple = apple + in_apple
+    deal = deal + 1 
+    print("你現在有",money,"元")
+    print("你現在有"‚apple,"顆蘋果") 
+if q == 3
+    out_apple = (int(input("請問今天出貨了幾顆蘋果")))
+    money = money + (apple * spend) 
+    apple = apple - out_apple  
+    deal = deal + 1
+    print("你現在有",money,"元")
+    print("你現在有"‚apple,"顆蘋果")
+
